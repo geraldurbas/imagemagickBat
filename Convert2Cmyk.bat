5bf55edc0e790e7f00f3fbf786fd592b940be873
Gerald Urbas <121263+geraldurbas@users.noreply.github.com>
2019-05-22 01:57:07 +0200

    Create Convert2Cmyk.bat

---
+++ Convert2Cmyk.bat
@@ -0,0 +1,31 @@
+@echo off
+
+
+SET identifyprog="C:\ImageMagick-7.0.8-46-portable-Q16-x64\identify"
+SET magickprog="C:\ImageMagick-7.0.8-46-portable-Q16-x64\magick"
+
+SET imagepath="dir /B /S C:\Orig\*.jpg"
+
+set SEARCHTEXT="Orig"
+set REPLACETEXT="Conv"
+
+
+
+for /F "delims=" %%I in ('%imagepath%') do call :process "%%I"
+goto :eof
+
+:process
+%identifyprog% -verbose %1 | find /c /i "Colorspace: sRGB" > temp.txt
+set /p count=<temp.txt
+del temp.txt
+if %count%==1 (
+SET "outpath=%~dp1"
+setlocal EnableDelayedExpansion
+SET modified=!outpath:BilderOrig=BilderConv!
+echo !modified!
+echo "%~dp1!
+mkdir !modified!
+echo !modified!%~n1.jpg!
+"%magickprog%" -verbose %1 -profile eciRGB_v2.icc -profile eciCMYK.icc !modified!%~n1.jpg!
+)
+goto :eof
